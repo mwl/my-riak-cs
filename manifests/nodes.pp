043493649c57963b7c90f281b44c9b3fa85e50a8
Martin Westergaard Lassen <martin@mwl.dk>
2012-05-17 17:20:41 +0200

    All riaks configured with riak-cs and one with stanchion

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,19 +1,40 @@
 node "riak10" {
     class {'riak':
-        riakhost => $ipaddress_eth1
+        riakhost => $ipaddress_eth1,
+        backend_profile => "cs",
+    }
+
+    class {'riakcs::stanchion':
+        stanchionhost => $ipaddress_eth1,
+    }
+
+    class {'riakcs':
+        riakcshost => $ipaddress_eth1,
     }
 }
 
 node "riak11" {
     class {'riak':
         riakhost => $ipaddress_eth1,
-        riak_ring => "riak@192.168.33.10"
+        riak_ring => "riak@192.168.33.10",
+        backend_profile => "cs",
+    }
+
+    class {'riakcs':
+        riakcshost => $ipaddress_eth1,
+        stanchion_ip => "192.168.33.10",
     }
 }
 
 node "riak12" {
     class {'riak':
         riakhost => $ipaddress_eth1,
-        riak_ring => "riak@192.168.33.10"
+        riak_ring => "riak@192.168.33.10",
+        backend_profile => "cs",
+    }
+
+    class {'riakcs':
+        riakcshost => $ipaddress_eth1,
+        stanchion_ip => "192.168.33.10",
     }
 }
